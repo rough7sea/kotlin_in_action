c34ed8b1685d8b066d84bd1f4886eb8b938ca442
danila.karnatsevich <danila.karnatsevich@taskdata.ru>
2021-01-12 19:44:29 +0300

    add data modificator

--- src/main/kotlin/chapter_4/compiler_generated_method/Main.kt
+++ src/main/kotlin/chapter_4/compiler_generated_method/Main.kt
@@ -10,12 +10,24 @@ class Client(val name: String, val postalCode: Int){
     }
 
     override fun hashCode(): Int = name.hashCode() * 31 + postalCode
+
+    fun copy(name: String = this.name,
+             postalCode: Int = this.postalCode) = Client(name, postalCode)
 }
+
+data class Client1(val name: String, val postalCode: Int)
 
 fun main() {
     val client1 = Client("Alice", 35424)
     val client2 = Client("Alice", 35424)
     println(client1)
+    println(client1.copy(postalCode = 123))
     println(client1 == client2)
+
+    val cl1 = Client1("Alice", 35424)
+    val cl2 = Client1("Alice", 35424)
+    println(cl1)
+    println(cl1.copy(postalCode = 123))
+    println(cl1 == cl2)
 
 }
